be2663322155012ea1a9e0a3f65e78276532037e
Michal Strnad <michal.strnad@nic.cz>
2018-01-17 06:39:29 +0100

    Apply workaround for bug in boost 1.58

--- src/getdns/data.cc
+++ src/getdns/data.cc
@@ -26,6 +26,7 @@
 #include <sstream>
 
 #include <boost/variant.hpp>
+#include <boost/version.hpp>
 #include <boost/archive/iterators/base64_from_binary.hpp>
 #include <boost/archive/iterators/binary_from_base64.hpp>
 #include <boost/archive/iterators/transform_width.hpp>
@@ -489,8 +490,16 @@ struct ValueIsOf:boost::static_visitor<bool>
     bool operator()(const X&)const { return false; }
 };
 
+//see https://svn.boost.org/trac10/ticket/11285
+#define APPLY_BOOST_1_58_BUG_WORKAROUND \
+    defined BOOST_VERSION && (105800 <= BOOST_VERSION) && (BOOST_VERSION < 105900)
+
 template <typename T>
+#if APPLY_BOOST_1_58_BUG_WORKAROUND
 struct GetValueOf
+#else
+struct GetValueOf:boost::static_visitor<const T&>
+#endif
 {
     const T& operator()(const T& _value)const { return _value; }
     template <typename X>
